c098b2cedf529df847d7af2ad063cd4719ff599e
Kalpesh Borde <bordekalpesh@gmail.com>
2025-12-01 00:17:34 +0530

    add signup, signin endpoints

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,14 +1,65 @@
 
 
 import express from 'express';
+import mongoose from 'mongoose';
+import { SignupSchema } from 'common/types';
+import { UserModel } from 'db/client';
+import jwt from 'jsonwebtoken';
+mongoose.connect(process.env.MONGO_URI!);
+
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 const app = express();
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  
+app.post("/signup", async (req, res) => {
+
+  const{ success, data } = SignupSchema.safeParse(req.body);
+  if (!success) {
+    res.status(403).json({ error: "Invalid input" });
+    return;
+
+  }
+
+  try {
+    const user = await UserModel.create({
+      username: data.username,
+      password: data.password,
+  })
+  res.json({
+    id: user._id
+  })
+  } catch (error) {
+    res.status(411).json({ message: "Username already exists" });
+  }
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
+  const { success, data } = SignupSchema.safeParse(req.body);
+  if (!success) {
+    res.status(403).json({ error: "Invalid input" });
+    return;
+  }
+
+  const user = await UserModel.findOne({
+    username: data.username,
+    password: data.password
+  })
+  if (user) {
+    const token = jwt.sign({
+      id: user._id,
+    }, JWT_SECRET);
+
+    res.json({ 
+      id: user._id,
+      token
+    });
+  } else {
+    res.status(403).json({ 
+      error: "Invalid credentials" 
+    });
+  }
+
   
 });
 
